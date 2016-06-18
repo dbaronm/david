b515bd6286eb4bdf03efbd3a54ddd9d84c281c9b
David <david.baron@softtek.com>
2016-06-17 21:02:44 -0500

    up to step 15

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,17 @@
 class PagesController < ApplicationController
+	def index
+		@nombre = params[:nombre]
+		@email = params[:email]
+		@edad = params[:edad]
+	end
+	def guardar_registro
+		@nombre = params[:nombre]
+		@email = params[:email]
+		@edad = params[:edad]
+		unless params[:nombre].blank? && params[:email].blank? && params[:edad].blank?
+			Registro.create(nombre:params[:nombre],email:params[:email],edad:params[:edad])
+			redirect_to pages_x_path, 
+			notice: "El usuario fue guardado"
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#x'
+  get 'pages/x'
+  post 'pages/x', action: 'guardar_registro', controller: 'pages', as: 'algo'
 end
